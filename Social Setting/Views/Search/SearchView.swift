14dbfbbbb2db69ee21a4d8e627d0f8d18457115b
jonathandowdell <jdowde2@wgu.edu>
2022-12-11 01:10:39 -0600

    Added SearchView

---
+++ Social Setting/Views/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Social Setting
+//
+//  Created by Mettaworldj on 12/11/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
