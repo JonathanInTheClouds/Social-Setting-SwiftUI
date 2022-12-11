12e0d84c2f302ef4234ac067c0bc8fc3f60dcfbe
jonathandowdell <jdowde2@wgu.edu>
2022-12-11 01:11:03 -0600

    Added SettingsView

---
+++ Social Setting/Views/Settings/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Social Setting
+//
+//  Created by Mettaworldj on 12/11/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
