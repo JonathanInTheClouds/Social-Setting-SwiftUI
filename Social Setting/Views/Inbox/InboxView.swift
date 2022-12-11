adc29474705ff00b2476ca0fc8b1b5918a7769f0
jonathandowdell <jdowde2@wgu.edu>
2022-12-11 01:06:40 -0600

    Added InboxView

---
+++ Social Setting/Views/Inbox/InboxView.swift
@@ -0,0 +1,20 @@
+//
+//  InboxView.swift
+//  Social Setting
+//
+//  Created by Mettaworldj on 12/11/22.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        Text("InboxView")
+    }
+}
+
+struct InboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InboxView()
+    }
+}
